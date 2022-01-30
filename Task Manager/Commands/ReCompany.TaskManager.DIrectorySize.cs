fe513eed0461bc000d14fffc7ba993509a79a0a4
Anatolii Revutskyi <revutskyanatoly@gmail.com>
2022-01-30 16:48:36 +0200

    Task Manager

---
+++ Task Manager/Commands/ReCompany.TaskManager.DIrectorySize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task_Manager
+{
+    internal class DirectorySize
+    {
+        public static void Directory_Size()  // Информация про папку
+        {
+            try
+            {
+                Console.WriteLine("Введите адресс папки, информацию о которой хотите узнать: ");
+                var answer = Console.ReadLine();
+                var files = Directory.EnumerateFiles(answer);
+                long sum = (from file in files let fileInfo = new FileInfo(file) select fileInfo.Length).Sum();
+                Console.WriteLine(answer + "    Размер: " + sum + " bytes" +
+                                                                  "   Дата создания: " + Directory.GetCreationTime($@"{answer}"));
+            }
+            catch
+            {
+                Console.WriteLine("Адресс введён неверно");
+            }
+        }
+    }
+}
---
+++ Task Manager/Commands/ReCompany.TaskManager.DiskInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Task_Manager
+{
+    internal class DirInfo
+    {
+        public static void Tree()   //Отрытие дерева диска 
+        {
+            try
+            {
+                Console.WriteLine("Введите диск или адресс папки: ");
+                var answer = Console.ReadLine();
+                var directory = new string[] { };
+                directory = Directory.GetFileSystemEntries($@"{answer}");
+                for (int i = 0; i < directory.Length; i++)
+                {
+                    Console.WriteLine(directory[i] + @$"    
+                                                        {Directory.GetCreationTime(directory[i])}");
+                }
+                Console.WriteLine("__________________________________________________________________");
+                Console.WriteLine("__________________________________________________________________");
+            }
+            catch
+            {
+                Console.WriteLine("Адресс введён неверно");
+            }
+        }
+    }
+}
